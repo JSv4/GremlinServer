e3643b4cf46ba87df846867c30cf198c54dc9c0d
JSv4 <scrudato@umich.edu>
2020-07-20 21:38:42 -0400

    Added CORS_ORIGIN_ALLOW_ALL again.

--- config/settings/base.py
+++ config/settings/base.py
@@ -398,6 +398,7 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 ]
 
 CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:8001",
     "http://localhost:3000",
