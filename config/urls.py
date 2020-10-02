dd8a068274b8bd22b571029f408ddfec144f8e4c
JSv4 <scrudato@umich.edu>
2020-10-02 00:18:07 -0400

    Built backend splashpage. Also got super basic swagger page running, though docs need a lot of work.

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework.schemas import get_schema_view
 from django.views import defaults as default_views
 from django.views.generic import TemplateView
 from django.contrib import admin
@@ -11,17 +10,31 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
     TokenVerifyView
 )
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# API documentation
+schema_view = get_schema_view(
+   openapi.Info(
+      title="GREMLIN API",
+      default_version='v1',
+      description="The low-code, microservices API",
+      terms_of_service="Coming soon... In the meantime, no warranties, express or implied and provided totally as-is...",
+      contact=openapi.Contact(email="scrudato@umich.edu"),
+      license=openapi.License(name="GPLv3"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
-    path(
-        "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
-    ),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
-    # User management
-    path("users/", include("gremlin_gplv3.users.urls", namespace="users")),
-    path("accounts/", include("allauth.urls")),
+  # User management
+  path("users/", include("gremlin_gplv3.users.urls", namespace="users")),
+  path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
@@ -35,15 +48,9 @@ urlpatterns += [
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('auth/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('auth/', include('config.auth_router')),
-    path('openapi/', get_schema_view(
-        title="Gremlin Microservice Platform",
-        description="The open-source, chainable microservices platform.",
-        version="1.0.0"
-    ), name='openapi-schema'),
-    path('swagger-ui/', TemplateView.as_view(
-        template_name='pages/swagger-ui.html',
-        extra_context={'schema_url':'openapi-schema'}
-    ), name='swagger-ui')
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 if settings.DEBUG:
