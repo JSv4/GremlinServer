b31fb17648381108c970034ca0b44ba056866067
John Scrudato <scrudato@umich.edu>
2021-01-24 10:51:58 -0500

    Added license to individual source files to complement existing license notices in readme.rst and license.rst

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,4 +1,22 @@
 """
+Gremlin - The open source legal engineering platform
+Copyright (C) 2020-2021 John Scrudato IV ("JSIV")
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/
+"""
+
+"""
 WSGI config for GREMLIN GPLv3 project.
 
 This module contains the WSGI application used by Django's development server
